8dc1f016e3bdcf8b2bac5a078e5c7d570724bb09
David <david.sierra@innocv.com>
2018-11-20 23:47:27 +0100

    add caching and rest template bean for invoke rest api

--- src/main/java/com/github/repositories/GithubRepositoriesApplication.java
+++ src/main/java/com/github/repositories/GithubRepositoriesApplication.java
@@ -2,11 +2,23 @@ package com.github.repositories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class GithubRepositoriesApplication {
+@EnableCaching
+public class GithubRepositoriesApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubRepositoriesApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
